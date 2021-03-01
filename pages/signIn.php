3333f87cc04e08861dc24689900c81e2ec9d9474
ChloeArd <chloe.ardoise@gmail.com>
2021-03-01 09:38:28 +0100

    Fait le responsive

--- pages/signIn.php
+++ pages/signIn.php
@@ -4,7 +4,7 @@ $title = "IMDb Marvel  sign-In";
 include "../_partials/header.php";
 ?>
 
-    <img id="logo_IMDb2" src="https://cdn.discordapp.com/attachments/689017273050202134/813127452494397440/logo_small.png" alt="logo IMDb">
+<a class="placeLogo" href="../index.php"><img id="logo_IMDb2" src="https://cdn.discordapp.com/attachments/689017273050202134/813127452494397440/logo_small.png" alt="logo IMDb"></a>
     <section id="createAccount" class="flexColumn">
         <h2 class="colorGrey titleAccount">Sign-In</h2>
         <form method="post" action=" " class="flexColumn formAccount">
